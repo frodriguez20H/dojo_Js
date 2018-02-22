9799dea29088c3e4651003738878527b6d491290
Fabián <frodriguez@20H.es>
2018-02-22 02:21:56 +0000

    Update Challenge 4.

--- Challenge 4/script_c4.js
+++ Challenge 4/script_c4.js
@@ -38,12 +38,6 @@ var QuestionBuilder = function(question, answers, correct) {
     this.correct = correct;
 }
 
-/*
-var question1 = "¿Javascript es un lenguaje de programación?";
-var answers1 = ['Sí', 'No'];
-var correct1 = 'Sí';
-*/
-
 var quest1 = new QuestionBuilder('¿Javascript es un lenguaje de programación?', ['Sí', 'No'], 'Sí');
 var quest2 = new QuestionBuilder('¿De qué color es el caballo blanco de Santiago?', ['Trasparente', 'Negro', 'Blanco'], 'Blanco');
 var quest3 = new QuestionBuilder('¿Qué pokemon usaría Yune?', ['Charmander', 'Pikachu', 'Saiduck', 'No sé'], 'Saiduck');
@@ -52,43 +46,17 @@ var quest5 = new QuestionBuilder('¿Are you sure need learn Javascript?', ['No',
 
 var questions = [quest1, quest2, quest3, quest4, quest5];
 
-console.log(quest1);
-console.log(questions);
-
 (function quest(arrayq){
     var postq = Math.floor(Math.random() * 5);  // Random position in array questions.
     var answers = [];
     var text = '';
-    console.log ('Posición del array ' + postq);
-    console.log('Contenido de objeto mirado ' + arrayq[postq]);
-    console.log('¿Ve el contenido: ' + arrayq[postq].question);
-    console.log('Longitud del array[1] = ' + arrayq[postq].answers.length);
     answers = arrayq[postq].answers;
-    console.log(answers);
-    console.log('Question = ' + arrayq[postq].question);
     text = arrayq[postq].question + '\n';
-    console.log('Tamaño array respuestas: ' + answers.length);
     for (var i = 0; i < answers.length; i++){
-            console.log('Array respuesta ' + i +': ' + answers[i])
             text = text + i + ' => ' + answers[i] + '\n';
         }
-    console.log('Contenido de text = ' + text);
 
-    /*
-    function genanswer(arranswers){
-        var long = arranswers.lenght;
-        for (l = 0; l < long; l++){
-            prompt('l =>' + answers[l]);
-        }
-    }
-    */
-
-    answers = arrayq[postq].answers;
-    console.log('Respuestas: ' + answers);
-    console.log('Contenido de text despues de ejecutarse: ' + text);
     var answer = prompt(text);
-    console.log('Respuesta correcta: ' + arrayq[postq].correct);
-    console.log('Respuesta dada= '+ answer);
     if (answers[answer] == arrayq[postq].correct){
         console.log('CORRECTO.');
         alert('¡Correcto!');
